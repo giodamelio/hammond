c9287c1968dd2f9ba30e8f393edcbe9baff87667
Giovanni d'Amelio <gio@damelio.net>
2025-11-24 12:06:02 -0800

    Super basic Discord bot

--- src/hammond/main.py
+++ src/hammond/main.py
@@ -1,5 +1,28 @@
+import discord
+
+from hammond.systemd_creds import SystemdCreds
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        _ = await message.channel.send("Hello!")
+
+
 def main():
-    print("Hello World")
+    client.run(SystemdCreds().discord_token)
 
 
 if __name__ == "__main__":
